19e1d74835a962903db5ec5470d2755a1e5995f5
Kareem Grant <kareem@getuserwise.com>
2016-07-25 11:53:44 -0400

    adds github links

--- src/components/GameContainer.jsx
+++ src/components/GameContainer.jsx
@@ -10,7 +10,15 @@ class GameContainer extends React.Component {
 
   render() {
     return (
-      <Game {...this.state} />
+      <div>
+        <Game {...this.state} />
+
+        <div className="attribution">
+          <p>
+            Made with &#x26a1; by <a href="https://github.com/kareemgrant" target="_blank">Kareem Grant</a> <a href="https://github.com/kareemgrant/react-simon-the-game" target="_blank">(code)</a> using <a href="https://facebook.github.io/react/" target="_blank">React</a> and <a href="http://redux.js.org/" target="_blank">Redux</a>
+          </p>
+        </div>
+      </div>
     );
   }
 }
